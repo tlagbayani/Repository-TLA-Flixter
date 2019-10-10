c16ad3be3996c3bbaffdaf79f9fc8d4917ac7626
tlagbayani <tlagbayani@gmail.com>
2019-10-10 21:40:09 +0000

    Applied changes for Lesson 20 Finalizing Permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -7,9 +7,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def create
-    if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
-    end
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
@@ -24,7 +21,7 @@ class Instructor::LessonsController < ApplicationController
 
   helper_method :current_section
   def current_section
-    @current_section ||= Section.find(params[section_id])
+    @current_section ||= Section.find(params[:section_id])
   end
 
   def lesson_params
