50fb3b1033520cbf83ce873147452148536d4186
tlagbayani <tlagbayani@gmail.com>
2019-11-08 04:55:30 +0000

    Applied Changes for Lesson 29 Payments with Stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]  
+
   def show
     
   end
 
 
   private
+
+def require_authorized_for_current_course
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You have to be enrolled in the course to view the lessons."
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
